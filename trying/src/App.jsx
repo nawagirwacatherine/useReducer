b63fea5f42272046b137e4cc7fc2d98c58e45b8f
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-10-06 01:39:52 +0200

    added reducer

--- trying/src/App.jsx
+++ trying/src/App.jsx
@@ -1,5 +1,12 @@
 import {useReducer} from "react"
 
+
+const reducer = (state, action) => {
+  if (action.type === 'buy-ingredients') return {money:state.money -10};
+  if (action.type ==='sell-a-meal') return {money: state.money + 10};
+  if (action.type ==="celebrity-visit")
+}
+
 function App() {
   
 
