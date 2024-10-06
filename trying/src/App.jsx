0f538206467d02d7c14ae11f7352de4d206a7df5
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-10-07 01:35:18 +0200

    Added buttons

--- trying/src/App.jsx
+++ trying/src/App.jsx
@@ -4,17 +4,23 @@ import {useReducer} from "react"
 const reducer = (state, action) => {
   if (action.type === 'buy-ingredients') return {money:state.money -10};
   if (action.type ==='sell-a-meal') return {money: state.money + 10};
-  if (action.type ==="celebrity-visit")
+  if (action.type ==="celebrity-visit") return {money:state.money+500};
+  return state;
 }
 
 function App() {
-  
+  const initialstate ={money: 100};
+  const [state,dispatch] = useReducer(reducer,initialstate);
 
   return (
-    <>
-     
-    </>
-  )
+    <div className="App">
+      <h1>Wallet: {state.money}</h1>
+      <div>
+        <button onClick={() => dispatch({type: 'buy-ingredients'})}>Shopping for veggies!</button>
+        <button onClick={() => dispatch({type: 'sell-a-meal'})}>Serve a meal to the customer</button>
+      </div>
+    </div>
+  );
 }
 
 export default App
